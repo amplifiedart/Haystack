577b74d4f00b021ae78a38ac6e8c959e94774227
Arjo Kalisvaart <amplifiedart@gmail.com>
2018-07-28 15:10:05 +0200

    Created the skeleton of the project based on metro UI 4.0.

---
+++ HS.Model/HSDocument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HS.Model
+{
+	public class HSDocument
+	{
+		public int DocumentId { get; set; }
+
+	}
+}
---
+++ HayStackMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HayStackMVC.MvcApplication" Language="C#" %>
